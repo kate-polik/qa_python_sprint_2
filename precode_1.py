0f441b8a626b3d1b9cd99083e0091b70a6f9baeb
Ekaterina Polikarpova <katya.polikarpovaa@gmail.com>
2024-11-20 05:00:24 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	precode_1.py
    #	precode_2.py
    #	precode_3.py

--- precode_1.py
+++ precode_1.py
@@ -6,4 +6,15 @@ from miles import HobbitsPriorityStatus
 class TestHobbitsPriorityStatus:
 
     # добавь декоратор
+    @pytest.mark.parametrize(
+        'hobbit, priority_status',
+        [
+            ['Фродо', 'Platinum'],
+            ['Сэм', 'Gold'],
+            ['Мэри', 'Silver'],
+            ['Пиппин', 'Silver']
+        ]
+    )
+    # передай параметры из декоратора в функцию
+    def test_priority_status(self, hobbit, priority_status):
         assert HobbitsPriorityStatus().get_status(hobbit) == priority_status
--- precode_2.py
+++ precode_2.py
@@ -8,3 +8,9 @@ class TestHobbitsPriorityStatus:
     def test_add_miles_and_check_status(self):
         # использовали объект класса HobbitsPriorityStatus, чтобы вызвать его методы
         hobbits_priority = HobbitsPriorityStatus()
+
+        hobbit = 'Голлум'
+        hobbits_priority.add_new_hobbit(hobbit)
+        hobbits_priority.add_miles_to_hobbit(hobbit, 50)
+
+        assert hobbits_priority.get_status(hobbit) == 'Classic'
